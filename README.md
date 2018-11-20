6931f97cb4fb27df57e170ffcb2f1cf9bea02096
James Ellwood <jellwood@gmail.com>
2018-11-19 23:13:02 -0500

    Move data dirs to mounts and get lazy with the es index.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wof-docker
+
+This is a very hacky attempt at getting a dockerized version of [Who's on First](https://github.com/whosonfirst) using the 'WOF in a Box' blog posts on https://www.whosonfirst.org, give or take. Very much a work in progress.
--- docker/wof-indexer/scripts/start.sh
+++ docker/wof-indexer/scripts/start.sh
@@ -2,7 +2,12 @@
 
 ELASTICSEARCH_URL="http://${ELASTICSEARCH_HOST}:9200"
 
-ls /usr/local/data
+if [ -f /usr/local/data/indexed ]; then
+    echo "Indexing already completed, last commit in data repo:"
+    cd /usr/local/data/whosonfirst-data
+    git show --name-only
+    exit 0
+fi
 
 cd /usr/local/data
 if [ ! -d  /usr/local/data/whosonfirst-data ]; then
@@ -16,18 +21,11 @@ if [ ! -d /usr/local/mapzen/es-whosonfirst-schema ]; then
     git clone https://github.com/lumberbarons/es-whosonfirst-schema.git
 fi
 
-if [ ! -f /usr/local/data/indexed ]; then
-    datestamp=$(date "+%Y.%m.%d")
-    index_name="spelunker_${datestamp}"
+cd /usr/local/mapzen/es-whosonfirst-schema
+cat schema/2.4/mappings.spelunker.json | curl -H "Content-Type: application/json" -X PUT ${ELASTICSEARCH_URL}/spelunker -d @-
 
-    cd /usr/local/mapzen/es-whosonfirst-schema
-    cat schema/2.4/mappings.spelunker.json | curl -H "Content-Type: application/json" -X PUT ${ELASTICSEARCH_URL}/${index_name} -d @-
-    curl -H "Content-Type: application/json" -X POST ${ELASTICSEARCH_URL}/_aliases -d "{ 'actions': [ { 'add': { 'alias': 'spelunker', 'index': '${index_name}' } } ] }"
-
-    cd /usr/local/data/whosonfirst-data
-    echo "Indexing data to: $ELASTICSEARCH_URL"
-    wof-es-index -m directory . --index=spelunker -b --host=${ELASTICSEARCH_HOST}
-    touch /usr/local/data/indexed
-fi
-
+cd /usr/local/data/whosonfirst-data
+echo "Indexing data to: $ELASTICSEARCH_URL"
+wof-es-index -m directory . --index=spelunker -b --host=${ELASTICSEARCH_HOST}
+touch /usr/local/data/indexed
 echo "Indexing complete"
